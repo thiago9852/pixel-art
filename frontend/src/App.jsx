5fe498231f39b30fb05de9032628f6be451f84b4
Thiago ferreira <thiagoferreira.113@gmail.com>
2025-12-15 23:25:18 -0300

    ui: frontend base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ws, setWs] = useState(null);
+  const canvasRef = useRef(null);
+  
+  const grid = useRef({}); 
+
+  const drawPixel = (ctx, x, y, color) => {
+    ctx.fillStyle = color;
+    ctx.fillRect(x * 10, y * 10, 10, 10);
+    grid.current[`${x}:${y}`] = color;
+  };
+
+  useEffect(() => {
+    // Conecta no Gateway
+    const socket = new WebSocket('ws://localhost:8000/ws');
+    const ctx = canvasRef.current.getContext('2d');
+
+    socket.onmessage = (event) => {
+      const msg = JSON.parse(event.data);
+
+      // Carga Inicial
+      if (msg.type === "init") {
+        msg.data.forEach((pixel) => {
+          drawPixel(ctx, pixel.x, pixel.y, pixel.color);
+        });
+      } 
+      // Alguém pintou
+      else {
+        drawPixel(ctx, msg.x, msg.y, msg.color);
+      }
+    };
+
+    setWs(socket);
+    return () => socket.close();
+  }, []);
+
+  const handleClick = (e) => {
+    const rect = canvasRef.current.getBoundingClientRect();
+    const x = Math.floor((e.clientX - rect.left) / 10);
+    const y = Math.floor((e.clientY - rect.top) / 10);
+    
+    // Envia para o Gateway
+    if (ws) {
+      ws.send(JSON.stringify({ x, y, color: "#ff0000ff" }));
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Pixel Art Distribuído</h1>
+      <canvas 
+        ref={canvasRef} 
+        width={500} 
+        height={500} 
+        style={{border: '1px solid black'}}
+        onClick={handleClick}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
