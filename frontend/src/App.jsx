16caedc9bdf41e0adf807d17462b2c71f0b73991
Thiago ferreira <thiagoferreira.113@gmail.com>
2025-12-17 09:36:23 -0300

    feat(ui): Adicionando PALETA de cores no canvas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import { useEffect, useRef, useState } from 'react';
 
+const PALETA = [
+  "#ff0000ff", "#00ff00ff", "#0000ffff", "#ffff00ff",
+  "#ff00ffff", "#00ffffff", "#000000ff", "#ffffffff"
+];
+
 function App() {
   const [ws, setWs] = useState(null);
   const canvasRef = useRef(null);
   
-  const grid = useRef({}); 
+  const [selectedColor, setSelectedColor] = useState(PALETA[0]);
 
   const drawPixel = (ctx, x, y, color) => {
     ctx.fillStyle = color;
     ctx.fillRect(x * 10, y * 10, 10, 10);
-    grid.current[`${x}:${y}`] = color;
   };
 
   useEffect(() => {
@@ -43,20 +47,49 @@ function App() {
     
     // Envia para o Gateway
     if (ws) {
-      ws.send(JSON.stringify({ x, y, color: "#ff0000ff" }));
+      ws.send(JSON.stringify({ x, y, color: selectedColor }) );
     }
   };
 
   return (
-    <div>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
       <h1>Pixel Art Distribuído</h1>
+      
+      {/* Barra de cores */}
+      <div style={{ 
+        display: 'flex', 
+        justifyContent: 'center', 
+        gap: '10px', 
+        marginBottom: '15px' 
+      }}>
+        {PALETA.map((color) => (
+          <div
+            key={color}
+            onClick={() => setSelectedColor(color)}
+            style={{
+              width: '40px',
+              height: '40px',
+              backgroundColor: color,
+              cursor: 'pointer',
+              border: selectedColor === color ? '2px solid #b65e00ff' : '1px solid #ccc',
+            }}
+          />
+        ))}
+      </div>
+
       <canvas 
         ref={canvasRef} 
         width={500} 
         height={500} 
-        style={{border: '1px solid black'}}
+        style={{
+          border: '1px solid black', 
+          cursor: 'crosshair',
+          backgroundColor: '#eee'
+        }}
         onClick={handleClick}
       />
+      
+      <p>Cor Selecionada: <span style={{color: selectedColor, fontWeight: 'bold'}}>{selectedColor}</span></p>
     </div>
   );
 }
